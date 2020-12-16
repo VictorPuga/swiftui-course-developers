2e35e4f05ed7776673796a03bdbb7b04c955154b
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2020-12-16 16:44:23 -0700

    Initial Commit

---
+++ Learn by Doing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn by Doing
+//
+//  Created by Víctor Manuel Puga Ruiz on 16/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Víctor Manuel Puga Ruiz on 16/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
