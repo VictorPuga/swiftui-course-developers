cf5755f4b32b32d6fcbbbddec777914927330906
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2020-12-16 17:51:13 -0700

    CardView

--- Learn by Doing/ContentView.swift
+++ Learn by Doing/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  // MARK: - Body
+  var body: some View {
+    CardView()
+  }
 }
 
+// MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
